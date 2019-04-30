678b154b88f482b80bf64ffa04af0097edfc5dc2
razzaksr <razzaksr@gmail.com>
2019-04-30 12:18:33 +0530

    mod2 added again in subdivison11

---
+++ mod2/AccessMod2.java
@@ -0,0 +1,12 @@
+package mod2;
+
+import mod1.Mejor;
+
+public class AccessMod2 extends Mejor
+{
+	public static void main(String[] args) 
+	{
+		AccessMod2 m=new AccessMod2();
+		m.bonding();m.addResource();//m.show();
+	}
+}
